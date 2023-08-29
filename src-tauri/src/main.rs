9f614dc73935391ba898052d03dc7757b6385526
Hitarashi <sayeed205@gmail.com>
2023-08-29 17:16:18 +0530

    Initial Commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,33 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+use window_vibrancy::apply_mica;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .setup(|app: &mut tauri::App| {
+            let window = app.get_window("main").unwrap();
+
+            #[cfg(target_os = "windows")]
+            apply_mica(&window, Some(true)).expect("Failed to apply mica");
+
+            window.minimize().unwrap();
+            window.unminimize().unwrap();
+
+            window.maximize().unwrap();
+            window.unmaximize().unwrap();
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
